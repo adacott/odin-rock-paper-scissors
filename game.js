7754cc01c83f645bab3cfd3600baa5235ac41187
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-15 19:42:11 +0000

    Added skeleton files and functions for game

---
+++ game.js
@@ -0,0 +1,25 @@
+// randomly returns rock, paper, or scissors
+function getComputerChoice(){
+
+}
+
+// function that plays a round; takes two parameters, the 
+// playerSelection (rock, paper, or scissors) and (computerSelection)
+// then it will return a string that declares the winner of thE round like so:
+// "You Lose! Paper beats Rock!"
+//
+// Hint: make your players playerselection parameter case-insensitive
+function playRound(playerSelection, getComputerChoice){
+    playerSelection = prompt("Please enter Rock, Paper, or Scissors: ");
+    // computer = getComputerChoice();
+
+    return console.log(`${playerSelection}`);
+}
+
+// Use the previous function inside of this one to play a 5-round game that keeps score and reports
+// a winner or a loser at the end. use prompt() to get input from the user
+function game(){
+
+}
+
+
