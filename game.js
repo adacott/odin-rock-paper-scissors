924b84a83b4f83f2b7813c1fb36b75dcaed56021
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-17 23:32:26 +0000

    Added eventlistener for buttons

--- game.js
+++ game.js
@@ -1,23 +1,23 @@
 // randomly returns rock, paper, or scissors
-function getComputerChoice(){
+function getComputerChoice() {
     /**
      * 
      * @param {int} min 
      * @param {int} max 
      */
-    function random(min, max){
+    function random(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
     let num = random(1, 3);
 
-    if(num == 1){
+    if (num == 1) {
         return "rock";
     }
-    else if(num == 2){
+    else if (num == 2) {
         return "paper";
     }
-    else{
+    else {
         return "scissors";
     }
 }
@@ -34,26 +34,26 @@ function getComputerChoice(){
  * @param {string} getComputerChoice 
  * @returns 
  */
-function playRound(){
+function playRound() {
     let player = prompt("Please enter Rock, Paper, or Scissors: ").toLowerCase();
     let computer = getComputerChoice();
 
-    if(player == "rock" && computer == "scissors"){
+    if (player == "rock" && computer == "scissors") {
         return "You win! Rock beats Scissors!";
     }
-    else if(player == "scissors" && computer == "paper"){
+    else if (player == "scissors" && computer == "paper") {
         return "You win! Scissors beat Paper!";
     }
-    else if(player == "paper" && computer == "rock"){
+    else if (player == "paper" && computer == "rock") {
         return "You win! Paper beats Rock!";
     }
-    else if(computer == "rock" && player == "scissors"){
+    else if (computer == "rock" && player == "scissors") {
         return "You lose! Rock beats Scissors!";
     }
-    else if(computer == "paper" && player == "rock"){
+    else if (computer == "paper" && player == "rock") {
         return "You lose! Paper beats Rock!";
     }
-    else if(computer == "scissors" && player == "paper"){
+    else if (computer == "scissors" && player == "paper") {
         return "You lose! Scissors beat Paper!";
     }
     else {
@@ -63,38 +63,63 @@ function playRound(){
 
 // Use the previous function inside of this one to play a 5-round game that keeps score and reports
 // a winner or a loser at the end. use prompt() to get input from the user
-function game(){
+function game() {
     let result, pscore = 0, cscore = 0, final;
+    result = playRound();
 
-    console.log("Okay, this game is to the best of 5 rounds: ");
-
-    for(let i = 0; i < 5; i ++){
-        result = playRound();
-        console.log(result);
-
-        if(result.includes("win")){
-            pscore += 1;
-        }
-        else if(result.includes("lose")){
-            cscore += 1;
-        }
-        else{
-            pscore += 1;
-            cscore += 1;
-        }
+    if (result.includes("win")) {
+        pscore += 1;
+    }
+    else if (result.includes("lose")) {
+        cscore += 1;
+    }
+    else {
+        pscore += 1;
+        cscore += 1;
     }
 
-    if(pscore > cscore){
+    if (pscore > cscore) {
         final = "The player wins!";
     }
-    else if(cscore > pscore){
+    else if (cscore > pscore) {
         final = "The computer wins!";
     }
-    else{
+    else {
         final = "It was a tie!";
     }
 
     return `The final score was: ${pscore} to ${cscore}. ${final}`;
 }
 
+
+// Always make sure that you have your script tag at the very end of your file. 
+// If you do not then try to use queryselector, it will not work properly because the DOM tree
+// has not been built and populated yet
+//
+// To add a custom function to this, instead use this format:
+// btns.forEach(btn => btn.addEventListener("click", myfunction()));
+
+// To see what you are working with you can create any console then log the event like so:
+// function playRound(e){
+//     console.log(e);
+// }
+// This will allow you to gain insight into the "click" event. This is functionally similar to defining function(e){} inside of the event listener function, but much cleaner while laying the ground-work for implementing a function that will actually do something
+
+const btns = document.querySelectorAll("button");
+console.log(btns);
+
+// Adds an event listener to each button, running the function for each
+// btns.forEach(btn => btn.addEventListener("click", function (e) {
+//     console.log(e);
+// }));
+
+function myFunction(e) {
+    player = document.querySelector("div.player");
+    console.log(player);
+
+    npc = document.querySelector("div.npc");
+    console.log(npc);
+}
+
+btns.forEach(btn => btn.addEventListener("click", myFunction));
 
